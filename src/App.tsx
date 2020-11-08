dc35f20ace6c5cee5ec4275ad03735c5956fb423
pwilson77 <p.o.yirenkyi@gmail.com>
2020-11-08 10:57:24 +0000

    Completed first part of UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.body}>
+        <ul className={styles.event_list}>
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+
+          <li className={styles.list_item}>
+            <img className={styles.avatar} src="#" alt="user-img" />
+            <div className={styles.text_container}>
+              <p className={styles.block_info}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quibusdam mollitia natus facere, fugiat necessitatibus
+                distinctio...
+              </p>
+              <p className={styles.date}>07, 2020</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
